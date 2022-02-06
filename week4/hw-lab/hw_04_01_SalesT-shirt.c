62124ae804b47e77cfbbc815bcdf084eb15bdb67
polawich <toniceee45@gmail.com>
2022-02-06 11:59:13 +0700

    edit code

--- week4/hw-lab/hw_04_01_SalesT-shirt.c
+++ week4/hw-lab/hw_04_01_SalesT-shirt.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 
 int main(){
-    int price,free,percent,ea,ea2,priceall;
+    int price,freecon,percent,ea,ea2,priceall;
     float discount1,discount2;
     scanf("%d\n%d\n%d",&price,&percent,&ea);
     //percent
     priceall = (price*ea);
     discount1 = (priceall-(priceall*percent)/100);
     //free
-    free = (ea/3);
-    ea2 = (ea-free);
+    freecon = (ea/3);
+    //printf("%d",free);
+    ea2 = (ea-freecon);
     discount2 = (price*ea2);
     //Output Debug
     //printf("%.2f\n%.2f\n",discount1,discount2);
@@ -17,7 +18,7 @@ int main(){
     if (discount1 < discount2){
         printf("Discount %d%%\n%.2f\n",percent,discount1);
     }
-    else if (discount2 < discount1 || discount2 == discount2){
+    else if (discount2 < discount1 || discount2 == discount1){
         printf("Buy 2 Get 1\n%.2f\n",discount2);
     }
     return 0;
---
+++ week4/hw-lab/test.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    float numa,cal;
+    scanf("%lf",&numa);
+    cal = numa % 3;
+    printf("%lf\n",cal);
+    return 0;
+}
