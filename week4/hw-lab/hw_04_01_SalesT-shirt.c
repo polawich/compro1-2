e0a417023a8e274c8993733967afc2a834fa6066
polawich <toniceee45@gmail.com>
2022-02-07 10:59:32 +0700

    edit code

--- week4/hw-lab/hw_04_01_SalesT-shirt.c
+++ week4/hw-lab/hw_04_01_SalesT-shirt.c
@@ -2,22 +2,19 @@
 #include<math.h>
 
 int main(){
-    int price,freecon,percent,ea,ea2,priceall;
+    float price,freecon,percent,ea,ea2,priceall;
     float discount1,discount2;
-    scanf("%d\n%d\n%d",&price,&percent,&ea);
+    scanf("%f\n%f\n%f",&price,&percent,&ea);
     //percent
     priceall = (price*ea);
     discount1 = (priceall-(priceall*percent)/100);
     //free
     freecon = floor(ea/3);
-    //printf("%d",free);
     ea2 = (ea-freecon);
     discount2 = (price*ea2);
-    //Output Debug
-    //printf("%.2f\n%.2f\n",discount1,discount2);
     //Condition
     if (discount1 < discount2){
-        printf("Discount %d%%\n%.2f\n",percent,discount1);
+        printf("Discount %.f%%\n%.2f\n",percent,discount1);
     }
     else if (discount2 < discount1 || discount2 == discount1){
         printf("Buy 2 Get 1\n%.2f\n",discount2);
