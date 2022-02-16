d0bfcb900ef150a964e2296b84d68a248bcfc57b
polawich <toniceee45@gmail.com>
2022-02-16 17:53:48 +0700

    edit code

---
+++ test.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int numa;
+    scanf("%d",&numa);
+    for (int i = 1; i <= numa; i++){
+        printf("%d ",i);
+    }
+    return 0;
+}
--- week6/hw-lab/Lab_06_01.c
+++ week6/hw-lab/Lab_06_01.c
@@ -3,24 +3,29 @@
 int main(){
     int M[] = {2, 20, 8, 10, 4, 6, 16, 18};
     int N[] = {1, 3, 9, 7, 11, 15, 19};
-    int numa;
-    scanf("%d",&numa);
-    if (numa <= 20 && numa >= 1){
-        for(size_t mindex = 0; mindex < 8; mindex++)
-        {
-            if (numa == M[mindex]){
-                printf("%d is in M at index [%d]",numa,mindex);
+    int numa,value=0;
+    while (1){
+        scanf("%d",&numa);
+        if (numa <= 20 && numa >= 1){
+            for(int mindex = 0; mindex < 8; mindex++){
+                if (numa == M[mindex]){
+                    printf("%d is in M at index [%d]",numa,mindex);
+                    value++;
+                    return 0;
+                }
+            }
+            for(int nindex = 0; nindex < 7; nindex++){
+                if (numa == N[nindex]){
+                    printf("%d is in N at index [%d]",numa,nindex);
+                    value++;
+                    return 0;
+                }
+            }
+            if (value==0){
+                printf("%d is not in neither M nor N",numa);
+                return 0;
             }
         }
-        for(size_t nindex = 0; nindex < 7; nindex++)
-        {
-            if (numa == N[nindex]){
-                printf("%d is in N at index [%d]",numa,nindex);
-            }
-        }
-        else{
-            printf("%d is not in neither M nor N",numa);
-        }  
     }
     return 0;
-} 
+}
