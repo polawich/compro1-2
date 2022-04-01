1fbcb2901e11883f2578baed8d08fcaa294d769c
polawich <toniceee45@gmail.com>
2022-04-01 16:53:19 +0700

    add file

--- week10/Lab_10-01_infobook.c
+++ week10/Lab_10-01_infobook.c
@@ -6,17 +6,19 @@ int main(){
         char name[60],sname[60];
         char sex[10];
         int age;
-        char idnum[12];
+        int idnum;
         float gpa;
-    };
-    struct student_info info;
-    scanf ("%[^] %[^] %[^] %[^] %[^] %[^]",&info.name ,&info.sname ,&info.sex ,&info.age ,&info.idnum ,&info.gpa);
+    }info;
+    scanf("%[^] %[^] %[^] %[^] %[^] %[^]",&info.name ,&info.sname ,&info.sex ,&info.age ,&info.idnum, &info.gpa);
+    // scanf("%d",&info.age);
+    // printf("%d",info.age);
     // printf("%s",info.name); debug output
     if (strcmp(info.sex,"Male") == 0){
-        printf("Mr %c %s (%d) ID: %s GPA %f\n",info.name[0] ,info.sname, info.age, info.idnum, info.gpa);
+        printf("Mr %c %s ",info.name[0] ,info.sname);
+        printf("(%d) ID: %d GPA %lf\n",info.age ,info.idnum ,info.gpa);
     }
     if (strcmp(info.sex,"Female") == 0){
-        printf("Miss %c %s (%d) ID: %s GPA %f\n",info.name[0] ,info.sname, info.age, info.idnum, info.gpa);
+        printf("Miss %c %s (%d) ID: %d GPA %f\n",info.name[0] ,info.sname, info.age, info.idnum, info.gpa);
     }
     return 0;
 }
